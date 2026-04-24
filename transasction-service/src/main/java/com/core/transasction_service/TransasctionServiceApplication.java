64e6c089bbfae3c8025ed2eadd6fb73432ff27da
EdwardDiazR <edwardadriandiaz2020@gmail.com>
2026-04-23 22:13:39 -0400

    add microservices

---
+++ transasction-service/src/main/java/com/core/transasction_service/TransasctionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.core.transasction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransasctionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransasctionServiceApplication.class, args);
+	}
+
+}
