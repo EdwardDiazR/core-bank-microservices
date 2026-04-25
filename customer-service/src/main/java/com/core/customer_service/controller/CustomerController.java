8246350fc94d4fe8cd251b68983a8249da958f70
EdwardDiazR <edwardadriandiaz2020@gmail.com>
2026-04-25 12:30:47 -0400

    create customer controller

---
+++ customer-service/src/main/java/com/core/customer_service/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package com.core.customer_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customer")
+public class CustomerController {
+
+    public CustomerController(){}
+}
